654b5cff60f5f4737c7ae43bb1e0f3b76cd85add
Github-Saravanan <95275888+Github-Saravanan@users.noreply.github.com>
2024-07-09 16:48:59 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,19 +1,3 @@
-variable azure_subscription_id {
-  description = "Value for Azure Subscription"
-}
-
-variable azure_client_id {
- description = "Value for Azure Client ID"
-}
-
-variable azure_client_secret {
-    description ="Value for Azure CLient Secret"
-}
-
-variable azure_tenant_id {
-    description ="Value for Azure Client ID"
-}
-
 variable aws_region {
     description ="Value for AWS Region"
 }
@@ -58,35 +42,5 @@ variable "language" {
 
 variable "instance_type" {
   description = "Instance type for Elastic Beanstalk"
-  type        = string
-}
-
-variable "resource_group" {
-  description = "Resource Group Name"
-  type        = string
-}
-
-variable "azure_region" {
-  description = "Resource Group Location"
-  type        = string
-}
-
-variable "app_service_plan" {
-  description = "App Service Plan"
-  type        = string
-}
-
-variable "sku_tier" {
-  description = "SKU TIER"
-  type        = string
-}
-
-variable "sku_size" {
-  description = "SKU SIZE"
-  type        = string
-}
-
-variable "app_service" {
-  description = "App Service"
   type        = string
 }
