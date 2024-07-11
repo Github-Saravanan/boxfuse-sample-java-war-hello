ff534ce2148f511f30704490285f4c6d15e27f49
Github-Saravanan <95275888+Github-Saravanan@users.noreply.github.com>
2024-07-11 12:00:22 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,13 +2,6 @@ variable aws_region {
     description ="Value for AWS Region"
 }
 
-variable aws_access_key {
-    description ="Value for AWS Access Key"
-}
-
-variable aws_secret_key {
-    description ="Value for AWS Secret Key"
-}
 
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
