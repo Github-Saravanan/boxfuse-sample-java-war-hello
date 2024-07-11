a5e8f179f83613b819470116d382119e4f878d1b
Github-Saravanan <95275888+Github-Saravanan@users.noreply.github.com>
2024-07-11 14:05:38 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "elasticbeanstalk-us-east-1-339712811840"  # Replace with your S3 bucket name
-    key            = "statefile/dev/terraform.tfstate"
-    region         = "us-east-1"                  # Replace with your AWS region
+    bucket         = "awsbucket-us-west-2-07"  # Replace with your S3 bucket name
+    key            = "dev-statefile/terraform.tfstate"
+    region         = "us-west-2"                  # Replace with your AWS region
   }
 }
