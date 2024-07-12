6d246a7dd945cf3605eec1a1d4229d29d7444dd1
Github-Saravanan <95275888+Github-Saravanan@users.noreply.github.com>
2024-07-12 12:24:20 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,15 +1,3 @@
-data "aws_secretsmanager_secret_version" "creds" {
-  secret_id = "eb-creds"
-}
-
-locals {
-  eb_creds = jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string)
-  vpc_cidr      = local.eb_creds.vpc_cidr
-  subnet_1_cidr = local.eb_creds.subnet_1_cidr
-  subnet_2_cidr = local.eb_creds.subnet_2_cidr
-  instance_type = local.eb_creds.instance_type
-}
-
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
@@ -27,5 +15,10 @@ variable "subnet_2_cidr" {
 
 variable "instance_type" {
   description = "Instance type for Elastic Beanstalk"
+  type        = string
+}
+
+variable "language" {
+  description = "Solution stack name language for Elastic Beanstalk"
   type        = string
 }
