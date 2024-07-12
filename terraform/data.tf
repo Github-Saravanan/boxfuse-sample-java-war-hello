92cfd248cb5f307adaea57db7c7e0ee9f44e607b
Github-Saravanan <95275888+Github-Saravanan@users.noreply.github.com>
2024-07-12 12:32:44 +0530

    Update data.tf

--- terraform/data.tf
+++ terraform/data.tf
@@ -5,8 +5,4 @@ data "aws_secretsmanager_secret_version" "creds" {
 locals {
   eb_creds      = jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string)
   vpc_cidr      = local.eb_creds.vpc_cidr
-  subnet_1_cidr = local.eb_creds.subnet_1_cidr
-  subnet_2_cidr = local.eb_creds.subnet_2_cidr
-  instance_type = local.eb_creds.instance_type
-  language      = local.eb_creds.language
 }
