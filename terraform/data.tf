6b591c7c15ed9a1254e29013af68d46f64d8b99a
Github-Saravanan <95275888+Github-Saravanan@users.noreply.github.com>
2024-07-12 12:40:27 +0530

    Delete terraform/data.tf

--- terraform/data.tf
+++
@@ -1,8 +0,0 @@
-data "aws_secretsmanager_secret_version" "creds" {
-  secret_id = "eb-creds"
-}
-
-locals {
-  eb_creds      = jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string)
-  vpc_cidr      = local.eb_creds.vpc_cidr
-}
