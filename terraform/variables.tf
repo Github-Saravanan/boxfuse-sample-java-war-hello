2c9640337c2fafa28645d3bc9c50a7ee49b619f0
Github-Saravanan <95275888+Github-Saravanan@users.noreply.github.com>
2024-07-12 12:32:27 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,23 +2,3 @@ variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
 }
-
-variable "subnet_1_cidr" {
-  description = "CIDR block for the first Subnet"
-  type        = string
-}
-
-variable "subnet_2_cidr" {
-  description = "CIDR block for the second Subnet"
-  type        = string
-}
-
-variable "instance_type" {
-  description = "Instance type for Elastic Beanstalk"
-  type        = string
-}
-
-variable "language" {
-  description = "Solution stack name language for Elastic Beanstalk"
-  type        = string
-}
