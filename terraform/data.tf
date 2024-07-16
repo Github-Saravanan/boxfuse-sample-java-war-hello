2d3497eaf299defded1848cfe0702383ed8a556e
Github-Saravanan <95275888+Github-Saravanan@users.noreply.github.com>
2024-07-16 10:50:54 +0530

    Update data.tf

--- terraform/data.tf
+++ terraform/data.tf
@@ -4,37 +4,37 @@ data "aws_ssm_parameter" "aws_region" {
 }
 
 data "aws_ssm_parameter" "availability_zone_1" {
-  name = "availability_zone_1"
+  name = "/myapp/availability_zone_1"
 }
 
 data "aws_ssm_parameter" "availability_zone_2" {
-  name = "availability_zone_2"
+  name = "/myapp/availability_zone_2"
 }
 
 data "aws_ssm_parameter" "vpc_cidr" {
-  name = "vpc_cidr"
+  name = "/myapp/vpc_cidr"
 }
 
 data "aws_ssm_parameter" "subnet_1_cidr" {
-  name = "subnet_1_cidr"
+  name = "/myapp/subnet_1_cidr"
 }
 
 data "aws_ssm_parameter" "subnet_2_cidr" {
-  name = "subnet_2_cidr"
+  name = "/myapp/subnet_2_cidr"
 }
 
 data "aws_ssm_parameter" "language" {
-  name = "language"
+  name = "/myapp/language"
 }
 
 data "aws_ssm_parameter" "instance_type" {
-  name = "instance_type"
+  name = "/myapp/instance_type"
 }
 
 data "aws_ssm_parameter" "application_name" {
-  name = "application_name"
+  name = "/myapp/application_name"
 }
 
 data "aws_ssm_parameter" "environment_name" {
-  name = "environment_name"
+  name = "/myapp/environment_name"
 }
